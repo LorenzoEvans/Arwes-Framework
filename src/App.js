bc872ebf75166dd893a005e79ea9a567d62429e4
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-09-02 22:57:10 -0400

    Time to rewire.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Header, Project, Link, Words, Router } from 'arwes'
+import Panel from "./Panel";
 import { Route } from 'react-router-dom'
 import TAO from "./Archives/TAO";
 function App() {
   return (
     <div className="App">
-    <Route exact path={"/Ancient_Ones"} component={TAO} />
+      <Router>
+      <Route exact path={"/"} component={Panel}/>
+        <Route exact path={"/Ancient_Ones"} component={TAO} />
+      </Router>
     </div>
   );
 }
